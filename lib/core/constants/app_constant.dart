172eb1494f5639f70904eb4dcb27041d45d36d8b
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-08-06 21:43:57 +0700

    Add AppConstant class for application constants and onboarding titles

---
+++ lib/core/constants/app_constant.dart
@@ -0,0 +1,8 @@
+class AppConstant {
+  static const String nameApp = "Zenya";
+
+  static const List<String> titleOnboarding = [
+    "Stay informed with the latest headlines, anytime, anywhere.",
+    "Get news that matters — fast, clear, and in your pocket."
+  ];
+}
