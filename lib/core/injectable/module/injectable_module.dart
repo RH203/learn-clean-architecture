9fb69ac59eea4d4f34ae9cfe5a7b3e5bb701d4d8
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-08-10 19:23:37 +0700

    Add SharedPreferences dependency to InjectableModules for persistent storage

--- lib/core/injectable/module/injectable_module.dart
+++ lib/core/injectable/module/injectable_module.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
 import 'package:learn_clean_archi/core/env/env.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 @module
 abstract class InjectableModules {
@@ -8,4 +9,9 @@ abstract class InjectableModules {
   Dio get dio => Dio(
     BaseOptions(baseUrl: Env.BASE_URL, headers: {'Authorization': Env.API_KEY}),
   );
+
+  @preResolve
+  @lazySingleton
+  Future<SharedPreferences> get preferences async =>
+      await SharedPreferences.getInstance();
 }
