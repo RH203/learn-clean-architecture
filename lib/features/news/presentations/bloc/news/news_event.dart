326037ff886830a2431b2a7f0381b8ca07ce9f53
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-08-10 19:25:05 +0700

    Add FetchTopHeadlinesNews and GetNewsBasedOnCategoryEvent classes to manage news fetching events

--- lib/features/news/presentations/bloc/news/news_event.dart
+++ lib/features/news/presentations/bloc/news/news_event.dart
@@ -4,4 +4,11 @@ sealed class NewsEvent {
   const NewsEvent();
 }
 
-final class FetchTopHeadlinesNews extends NewsEvent{}
+final class FetchTopHeadlinesNews extends NewsEvent {}
+
+final class GetNewsBasedOnCategoryEvent extends NewsEvent {
+  final String category;
+  final String? countryCode;
+
+  GetNewsBasedOnCategoryEvent(this.category, this.countryCode);
+}
