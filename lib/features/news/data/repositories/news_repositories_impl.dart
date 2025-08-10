08a7cfdcaf9e5b02b5fd9fe1cea79cb6f00fdb1b
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-08-10 19:24:16 +0700

    Refactor NewsRepositoriesImpl to include countryCode parameter in getTopHeadlines and getNewsBasedOnCategory methods

--- lib/features/news/data/repositories/news_repositories_impl.dart
+++ lib/features/news/data/repositories/news_repositories_impl.dart
@@ -1,7 +1,7 @@
 import 'package:injectable/injectable.dart';
 import 'package:learn_clean_archi/features/news/data/datasources/remote/news_data_source.dart';
 import 'package:learn_clean_archi/features/news/domain/entity/article.dart';
-import 'package:learn_clean_archi/features/news/domain/repositories/news_repository.dart';
+import 'package:learn_clean_archi/features/news/domain/repositories/news_repositories.dart';
 
 @LazySingleton(as: NewsRepositories)
 class NewsRepositoriesImpl implements NewsRepositories {
@@ -10,8 +10,22 @@ class NewsRepositoriesImpl implements NewsRepositories {
   NewsRepositoriesImpl(this.newsRemoteDataSource);
 
   @override
-  Future<List<Article>> getTopHeadlines()async{
-    final model = await newsRemoteDataSource.getTopHeadlines();
+  Future<List<Article>> getTopHeadlines(String? countryCode) async {
+    final model = await newsRemoteDataSource.getTopHeadlines(countryCode);
+    return model.map((e) => e.toEntity()).toList();
+  }
+
+  @override
+  Future<List<Article>?> getNewsBasedOnCategory(
+    String category,
+    String? countryCode,
+  ) async {
+    final model = await newsRemoteDataSource.getNewsBasedOnCategory(
+      category,
+      countryCode,
+    );
+
+    if (model == null) return null;
     return model.map((e) => e.toEntity()).toList();
   }
 }
