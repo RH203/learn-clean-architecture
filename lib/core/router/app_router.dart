666dbadce8787077eacf9cfd1e90e3f711d857a8
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-08-10 19:23:41 +0700

    Refactor AppRouter to update routes and improve navigation; replace ListNewsPages with ListNewsWidget and adjust detail news route handling.

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,5 +1,8 @@
 import 'package:go_router/go_router.dart';
-import 'package:learn_clean_archi/features/news/presentations/pages/list_news_pages.dart';
+import 'package:learn_clean_archi/features/news/domain/entity/article.dart';
+import 'package:learn_clean_archi/features/news/presentations/pages/detail_news_pages.dart';
+import 'package:learn_clean_archi/features/news/presentations/pages/news_pages.dart';
+import 'package:learn_clean_archi/features/news/presentations/widgets/list_news_widget.dart';
 import 'package:learn_clean_archi/features/onboarding/presentation/pages/onboarding_pages.dart';
 
 class AppRouter {
@@ -9,13 +12,18 @@ class AppRouter {
     debugLogDiagnostics: true,
     initialLocation: '/',
     routes: [
-      GoRoute(
-        path: '/',
-        builder: (context, state) => OnboardingPages(),
-      ),
+      GoRoute(path: '/', builder: (context, state) => OnboardingPages()),
       GoRoute(
         path: '/list-news',
-        builder: (context, state) => ListNewsPages(),
+        builder: (context, state) => ListNewsWidget(),
+      ),
+      GoRoute(path: '/news', builder: (context, state) => NewsPages()),
+      GoRoute(
+        path: '/detail-news',
+        builder: (context, state) {
+          final Article news = state.extra as Article;
+          return DetailNewsPages(article: news);
+        },
       ),
     ],
   );
