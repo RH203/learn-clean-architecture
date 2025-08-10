cbb72f8ba271a700c57006f9350f5c319d0c7bc9
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-08-10 19:24:30 +0700

    Refactor NewsRepositories to include countryCode parameter in getTopHeadlines and getNewsBasedOnCategory methods

--- lib/features/news/domain/repositories/news_repositories.dart
+++ lib/features/news/domain/repositories/news_repositories.dart
@@ -1,5 +1,10 @@
 import 'package:learn_clean_archi/features/news/domain/entity/article.dart';
 
-abstract class NewsRepositories{
-  Future<List<Article>> getTopHeadlines();
+abstract class NewsRepositories {
+  Future<List<Article>> getTopHeadlines(String? countryCode);
+
+  Future<List<Article>?> getNewsBasedOnCategory(
+    String category,
+    String? countryCode,
+  );
 }
