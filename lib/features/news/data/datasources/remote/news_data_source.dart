b9724f55409bfef8192c203a302c7fea698d264c
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-08-10 19:24:04 +0700

    Refactor NewsRemoteDataSource to include countryCode parameter for getTopHeadlines and getNewsBasedOnCategory methods; update query parameters accordingly.

--- lib/features/news/data/datasources/remote/news_data_source.dart
+++ lib/features/news/data/datasources/remote/news_data_source.dart
@@ -1,10 +1,15 @@
 import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
-import 'package:learn_clean_archi/core/injectable/injectable.dart';
+import 'package:learn_clean_archi/app_logger.dart';
 import 'package:learn_clean_archi/features/news/data/model/article_model.dart';
 
 abstract class NewsRemoteDataSource {
-  Future<List<ArticleModel>> getTopHeadlines();
+  Future<List<ArticleModel>> getTopHeadlines(String? countryCode);
+
+  Future<List<ArticleModel>?> getNewsBasedOnCategory(
+    String category,
+    String? countryCode,
+  );
 }
 
 @LazySingleton(as: NewsRemoteDataSource)
@@ -14,13 +19,52 @@ class NewsRemoteDataSourceImpl implements NewsRemoteDataSource {
   NewsRemoteDataSourceImpl(this.dio);
 
   @override
-  Future<List<ArticleModel>> getTopHeadlines() async {
+  Future<List<ArticleModel>> getTopHeadlines(String? countryCode) async {
     final response = await dio.get(
       '/top-headlines',
-      queryParameters: {'country': 'us'},
+      queryParameters: {
+        'country': countryCode?.toLowerCase(),
+        'pageSize': '30',
+      },
     );
 
     final articles = response.data['articles'] as List;
+
+    return articles.map((e) => ArticleModel.fromJson(e)).toList();
+  }
+
+  Future<List<ArticleModel>?> getNewsBasedOnCategory(
+    String category,
+    String? countryCode,
+  ) async {
+    final response;
+
+    if (category != 'top headlines') {
+      response = await dio.get(
+        '/top-headlines',
+        queryParameters: {
+          'country': countryCode?.toLowerCase(),
+          'category': category,
+          'pageSize': '30',
+        },
+      );
+    } else {
+      response = await dio.get(
+        '/top-headlines',
+        queryParameters: {
+          'country': countryCode?.toLowerCase(),
+          'pageSize': '30',
+        },
+      );
+    }
+
+    final data = response.data;
+    AppLogger.debug(data);
+
+    if (data == null || data['articles'] == null) {
+      return [];
+    }
+    final articles = data['articles'] as List;
     return articles.map((e) => ArticleModel.fromJson(e)).toList();
   }
 }
